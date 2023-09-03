40871d1f5579a3d23daf40e430da07c2797a5a9d
Timuratmaca <115021588+Timuratmaca@users.noreply.github.com>
2023-09-03 20:57:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let ameliyatlar = document.getElementsByClassName("ameliyatlar")
+// let ameliyatlar = document.getElementsByClassName("ameliyatlar")
+// let animals = document.getElementsByClassName("animals")
+// for(let a=0;a<6;a=a+1){
+//     // * ameliyatlar[a].style.height=window.innerHeight+"px"
+//     // * animals[a].style.height=window.innerHeight-52+"px"
+// }
 
-ameliyatlar[0].style.height=window.innerHeight+"px"
+//  //console.log(window);
+alert("hs veteriner klinği")
+console.log("timur")
+
+let comments = document.getElementsByClassName("coments")
+
+var HELLO = "HELLO WORLD";
+
+// console.log  = print: HELLO;
